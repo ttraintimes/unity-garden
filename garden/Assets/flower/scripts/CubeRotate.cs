0d0d78d207d07fc1d1699fbfe3b2da7a250e5d7a
whereistt <mac@pc-129-31.customer.ask4.lan>
2021-02-23 23:10:22 +0000

    1

--- garden/Assets/flower/scripts/CubeRotate.cs
+++ garden/Assets/flower/scripts/CubeRotate.cs
@@ -5,30 +5,24 @@ using UnityEngine;
 public class CubeRotate : MonoBehaviour
 {
     // Public variable for turret turn speed
-   // public float turnSpeed = 5.0f; 
+    public float turnSpeed = 0.8f; 
     private GameObject MyCube;
-    private float d;
+   // private float d;
 
     // Start is called before the first frame update
     void Start()
     {
         MyCube = GameObject.Find("RotateCube");
-        d = (float)0.3;
+       
     }
 
     void Update()
     {
-        MyCube.transform.Rotate(0, 2*d, 0);
-       // float rotation = 0.0f;
-       // if (Input.GetKey(KeyCode.Q))
-       // {
-       //     rotation = -turnSpeed * Time.deltaTime;
-       // }
-       // else if (Input.GetKey(KeyCode.E))
-       // {
-       //     rotation = turnSpeed * Time.deltaTime;
-       // }
-       // Transform t = gameObject.transform;
-       // t.Rotate(0.0f, rotation, 0.0f);
+       // MyCube.transform.Rotate(0, d + Time.deltaTime, 0);
+        float rotation = 0.0f;
+        Transform t = MyCube.transform;
+        t.Rotate(0.0f, turnSpeed, 0.0f);
+      // turnSpeed++;
+        
     }
 }
